a8779cab3aa41fc5baee3b4cfe850e53bed4ea2a
Yaroslav <y.mashchenko0610@gmail.com>
2024-07-21 19:29:11 +0400

    hw-js-07

--- js/task-6.js
+++ js/task-6.js
@@ -6,16 +6,18 @@ function createBoxes(amount) {
   const boxesContainer = document.querySelector('#boxes');
   boxesContainer.innerHTML = '';
 
-  let size = 30;
+   const fragment = document.createDocumentFragment();
 
+  let size = 30;
   for (let i = 0; i < amount; i++) {
     const box = document.createElement('div');
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
+    fragment.appendChild(box);
     size += 10;
   }
+   boxesContainer.appendChild(fragment);
 }
 
 
